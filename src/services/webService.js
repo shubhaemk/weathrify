8b8b9abdcdb67d5f72d9d2c3ee573c9dfea3b23d
Shubham Kamath <dev.shubhaem@gmail.com>
2020-07-31 02:25:56 +0530

    feat: location fetch service implemented

--- src/services/webService.js
+++ src/services/webService.js
@@ -1,35 +1,71 @@
-import { getWeatherURL, getBackgroundURL } from '../services/urlService';
+import {
+  getWeatherURL,
+  getBackgroundURL,
+  getLocationSuggestion,
+} from '../services/urlService';
 
 const weatherFetch = (city) => {
-    return new Promise(async (resolvePromise, rejectPromise) => {
-        const FULL_URL = getWeatherURL(city);
-        const response = await fetch(FULL_URL);
-        if (response.status === 200) {
-            const cityWeather = await response.json();
-            resolvePromise(cityWeather);
-        } else {
-            const error = await response.text();
-            rejectPromise(JSON.parse(error));
-        }
-    })
-}
+  return new Promise(async (resolvePromise, rejectPromise) => {
+    const FULL_URL = getWeatherURL(city);
+    const response = await fetch(FULL_URL);
+    if (response.status === 200) {
+      const cityWeather = await response.json();
+      resolvePromise(cityWeather);
+    } else {
+      const error = await response.text();
+      rejectPromise(JSON.parse(error));
+    }
+  });
+};
 
-const backgroundFetch = blur => {
-    return new Promise(async (resolvePromise, rejectPromise) => {
-        const FULL_URL = getBackgroundURL(blur);
-        const response = await fetch(FULL_URL);
-        if(response.status === 200) {
-            const background = response.url;
-            resolvePromise(background);
-        } else {
-            rejectPromise();
-        }
-    })
-}
+const backgroundFetch = (blur) => {
+  return new Promise(async (resolvePromise, rejectPromise) => {
+    const FULL_URL = getBackgroundURL(blur);
+    const response = await fetch(FULL_URL);
+    if (response.status === 200) {
+      const background = response.url;
+      resolvePromise(background);
+    } else {
+      rejectPromise();
+    }
+  });
+};
+
+const locationFetch = (location) => {
+  return new Promise(async (resolvePromise, rejectPromise) => {
+    const FULL_URL = getLocationSuggestion(location);
+    const response = await fetch(FULL_URL, {
+      headers: {
+        Accept: 'application / vnd.teleport.v1 + json',
+      },
+    });
+
+    if (response.status === 200) {
+      const rawData = await response.json();
+      if (!rawData.count) {
+        rejectPromise({ error: 'not-found' });
+      } else {
+        const { _embedded } = rawData;
+        const searchResults = _embedded[`city:search-results`];
+        //console.log(searchResults);
+        const filteredData = searchResults.map((result) => {
+          return {
+            city: result[`matching_full_name`],
+          };
+        });
+        //console.log({ filteredData });
+        resolvePromise(filteredData);
+      }
+    } else {
+      rejectPromise();
+    }
+  });
+};
 
 export default {
   weatherFetch,
   backgroundFetch,
+  locationFetch,
 };
 
-export { weatherFetch, backgroundFetch };
+export { weatherFetch, backgroundFetch, locationFetch };
