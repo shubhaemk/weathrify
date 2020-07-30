53d7233716820f234bdf9203d1e5296cfe928d8b
Shubham Kamath <dev.shubhaem@gmail.com>
2020-07-31 02:23:18 +0530

    feat: left card design

---
+++ src/components/ExtraCardComponent/ExtraCard.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import LogoComponent from '../LogoComponent/LogoComponent';
+import CitySearchComponent from '../CItySearchComponent/CitySearch.component';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const ExtraCardComponent = (props) => {
+  return (
+    <Container>
+      <LogoComponent />
+      <CitySearchComponent />
+    </Container>
+  );
+};
+
+export default ExtraCardComponent;
