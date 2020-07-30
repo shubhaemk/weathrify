b9446bb7a8720fe646142d7db97e81ecb55e868c
Shubham Kamath <dev.shubhaem@gmail.com>
2020-07-31 02:22:57 +0530

    added: overflow hidden ;)

--- src/components/CardComponent/Card.styles.js
+++ src/components/CardComponent/Card.styles.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import { background, borderRadius } from '../../constants';
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    background-color: ${background.card};
-    border-radius: ${borderRadius};
+  background-color: ${background.card};
+  border-radius: ${borderRadius};
+
+  overflow: hidden;
 `;
