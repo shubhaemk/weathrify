86249c84c15cae2e977464869e0d228270b937ee
Shubham Kamath <dev.shubhaem@gmail.com>
2020-07-31 02:24:48 +0530

    fix: export

--- src/hooks/fetchDataHook.jsx
+++ src/hooks/fetchDataHook.jsx
@@ -1,31 +1,33 @@
-import { useState } from "react";
-import useDeepCompareEffect from "use-deep-compare-effect";
+import { useState } from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect';
 
-export const useFetchData = (service, ...args) => {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+const useFetchData = (service, ...args) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    const dataFetchSuccess = (data) => {
-        setData(data);
-        setLoading(false);
-    };
+  const dataFetchSuccess = (data) => {
+    setData(data);
+    setLoading(false);
+  };
 
-    const dataFetchFailure = (error) => {
-        setError(error);
-        setLoading(false);
-    };
+  const dataFetchFailure = (error) => {
+    setError(error);
+    setLoading(false);
+  };
 
-    useDeepCompareEffect(() => {
-      (async () => {
-        try {
-          const data = await service(...args);
-          dataFetchSuccess(data);
-        } catch (error) {
-          dataFetchFailure(error || { message: "error fetching data" });
-        }
-      })();
-    }, [service, args]);
+  useDeepCompareEffect(() => {
+    (async () => {
+      try {
+        const data = await service(...args);
+        dataFetchSuccess(data);
+      } catch (error) {
+        dataFetchFailure(error || { message: 'error fetching data' });
+      }
+    })();
+  }, [service, args]);
 
-    return [isLoading, data, error];
-}
+  return [isLoading, data, error];
+};
+
+export default useFetchData;
