6fe4e71f56850b77629b1f977bad5bc75b5b1900
Shubham Kamath <dev.shubhaem@gmail.com>
2020-07-31 02:24:39 +0530

    added: card background

--- src/constants/_variables.js
+++ src/constants/_variables.js
@@ -1,15 +1,16 @@
 //colors
 export const background = {
-    layer: "#202020",
-    card: "#000"
-}
+  layer: '#202020',
+  card: '#000',
+  secondary: 'rgba(126, 156, 168)',
+};
 
 //misc
-export const borderRadius= "1.5rem";
+export const borderRadius = '1.5rem';
 
 export const text = {
-    primary: "rgba(44, 97, 169)",
-    secondary: "rgba(126, 156, 168)",
-    light: "#eee",
-    mediumLight: "#cccc"
+  primary: 'rgba(44, 97, 169)',
+  secondary: 'rgba(126, 156, 168)',
+  light: '#eee',
+  mediumLight: '#cccc',
 };
